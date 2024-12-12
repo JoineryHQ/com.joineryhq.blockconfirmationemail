96ac2229148f53104a9cca59ea1fd15c4e27abf1
Allen Shaw <allen@JoineryHQ.com>
2024-12-12 10:56:18 -0600

    Upgrade codebase to civix format 23.02.1

--- blockconfirmationemail.php
+++ blockconfirmationemail.php
@@ -155,59 +155,12 @@ function blockconfirmationemail_civicrm_install() {
 }
 
 /**
- * Implements hook_civicrm_postInstall().
- *
- * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_postInstall
- */
-function blockconfirmationemail_civicrm_postInstall() {
-  _blockconfirmationemail_civix_civicrm_postInstall();
-}
-
-/**
- * Implements hook_civicrm_uninstall().
- *
- * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_uninstall
- */
-function blockconfirmationemail_civicrm_uninstall() {
-  _blockconfirmationemail_civix_civicrm_uninstall();
-}
-
-/**
  * Implements hook_civicrm_enable().
  *
  * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_enable
  */
 function blockconfirmationemail_civicrm_enable() {
   _blockconfirmationemail_civix_civicrm_enable();
-}
-
-/**
- * Implements hook_civicrm_disable().
- *
- * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_disable
- */
-function blockconfirmationemail_civicrm_disable() {
-  _blockconfirmationemail_civix_civicrm_disable();
-}
-
-/**
- * Implements hook_civicrm_upgrade().
- *
- * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_upgrade
- */
-function blockconfirmationemail_civicrm_upgrade($op, CRM_Queue_Queue $queue = NULL) {
-  return _blockconfirmationemail_civix_civicrm_upgrade($op, $queue);
-}
-
-/**
- * Implements hook_civicrm_entityTypes().
- *
- * Declare entity types provided by this module.
- *
- * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_entityTypes
- */
-function blockconfirmationemail_civicrm_entityTypes(&$entityTypes) {
-  _blockconfirmationemail_civix_civicrm_entityTypes($entityTypes);
 }
 
 // --- Functions below this ship commented out. Uncomment as required. ---
